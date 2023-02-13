27d1229c12243dff361584007a7684c140e76032
ConnorMCHARRY <4200184@schoolsnet.act.edu.au>
2023-02-13 14:44:00 +1100

    Global.gd update
    bullet updates

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,18 +13,18 @@ func _ready():
 	set_physics_process(true)
 
 func _process(delta):
-	if input. is_action_just_pressed("fire"):
+	if Input. is_action_just_pressed("fire"):
 		var bulletInstance = bulletSource.instance()
-		bulletInstance.postiion = Vector2(position.x, position.y-20)
+		bulletInstance.position = Vector2(position.x, position.y-20)
 		get_tree().get_root().add_child(bulletInstance)
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		move_and_collide(Vector2(-movement_speed * delta, 0))
 	if Input.is_action_pressed("ui_right"):
-		move_and_collide(Vector2(-movement_speed * delta, 0))
-		if Input.is_action_pressed("ui_up"):
+		move_and_collide(Vector2(movement_speed * delta, 0))
+	if Input.is_action_pressed("ui_up"):
 		move_and_collide(Vector2(0, -movement_speed * delta))
-		if Input.is_action_pressed("ui_down"):
-		move_and_collide(Vector2(0, -movement_speed * delta))
+	if Input.is_action_pressed("ui_down"):
+		move_and_collide(Vector2(0, movement_speed * delta))
 
