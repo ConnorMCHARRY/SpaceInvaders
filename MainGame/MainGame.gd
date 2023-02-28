0ebc169078eeec9863c47500a75341c042f61565
ConnorMCHARRY <4200184@schoolsnet.act.edu.au>
2023-03-01 09:48:31 +1100

    bullets reset

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -8,10 +8,12 @@ func _ready():
 	$HUD/Countdown.text = str(currentTimer)
 	
 	while currentTimer > 0:
-		yield(get_tree().create_timer(1.0), "timeout")
 		$HUD/Countdown.text = str(currentTimer)
+		yield(get_tree().create_timer(1.0), "timeout")
 		currentTimer = currentTimer - 1
 		print(currentTimer)
 	print("Game Over")
+	GlobalVariables.bulletInstanceCount = 0
 	get_tree().change_scene("res://Menu/Menu.tscn")
+	
 
