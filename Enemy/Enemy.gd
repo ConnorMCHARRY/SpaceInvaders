eea4e8cb5bfbab5908c02d8adf2e4ef74ed5a6bb
ConnorMCHARRY <4200184@schoolsnet.act.edu.au>
2023-03-24 12:59:24 +1100

    enemy shooting (in progress)

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,10 +1,28 @@
 extends KinematicBody2D
 
+var bullet = preload("res://Bullet-Enemy/Bullet-Enemy.tscn")
+
 func _ready():
-	$CollisionShape2D.connect("area_entered", self, "_colliding")
+	$Area2D.connect("area_entered", self, "_colliding")
 
 func _colliding(area):
 	print (area)
 	if area.is_in_group("right"):
 		get_parent().global_position.y += 10
 		get_parent().speed = get_parent().speed * -1
+	if area.is_in_group("left"):
+		get_parent().global_position.y += 10
+		get_parent().speed = get_parent().speed * -1
+
+func _process(delta):
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var my_random_number = rng.randf_range(2.0, 30.0)
+	yield(get_tree().create_timer(my_random_timer), "timeout")
+	if globalVariables.enemyBulletInstanceCount < 5:
+		var bulletInstance = bullet.Instance()
+		
+		bulletInstance.position = Vector2(global_position.x, global_position.y+20)
+		get_tree().get_root().add_child(bulletInstance)
+	
+	
